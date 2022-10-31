19df9edd3969115b0a83b96edf3af50250ab7ef4
Nebel MASS <Duordan.massengo@gmail.com>
2022-11-01 00:19:39 +0100

    Issue one fixed

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,12 +12,21 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+const closeModalBtn = document.querySelectorAll(".close");
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+closeModalBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
 
 
